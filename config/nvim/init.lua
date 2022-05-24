2c9c93644c815666df89e9bd659d31a183a1b57b
qiloz <sasha2108i@gmail.com>
2022-05-24 21:17:31 +0300

    Created repo

---
+++ config/nvim/init.lua
@@ -0,0 +1,14 @@
+----------------------
+---import lua modules---
+------------------------
+
+require('plugins')
+require('loader')
+require('settings')
+require('keymaps')
+require('lualine_conf')
+require('cmp_conf')
+
+-- LSP --
+-- require('lspconfig').pyright.setup{}
+-- require('lsp
---
+++ config/nvim/lua/plugins.lua
@@ -0,0 +1,34 @@
+
+-- _  ___   _____ __  __   ___ _   _   _  ___ _ ___ 
+--| \| \ \ / /_ _|  \/  | | _ \ | | | | |/ __( ) __|
+--| .` |\ V / | || |\/| | |  _/ |_| |_| | (_ |/\__ \
+--|_|\_| \_/ |___|_|  |_| |_| |____\___/ \___| |___/
+  
+local Plug = vim.fn['plug#']
+vim.call('plug#begin', '~/.config/nvim/plugged')
+
+----LUALINE-------------------------------------
+Plug 'nvim-lualine/lualine.nvim'
+----ICONS FOR LUALINE---------------------------
+Plug 'kyazdani42/nvim-web-devicons'
+----COMMENT-------------------------------------
+Plug 'numToStr/Comment.nvim' 
+----STARTUPTIME---------------------------------
+Plug 'dstein64/vim-startuptime'
+----LSPCONFIG-----------------------------------
+Plug 'neovim/nvim-lspconfig'
+----LSPINSTALLER--------------------------------
+Plug 'williamboman/nvim-lsp-installer'
+----NVIM-CMP------------------------------------
+Plug 'hrsh7th/cmp-nvim-lsp'
+Plug 'hrsh7th/cmp-buffer'
+Plug 'hrsh7th/cmp-path'
+Plug 'hrsh7th/cmp-cmdline'
+Plug 'hrsh7th/nvim-cmp'
+Plug 'hrsh7th/cmp-nvim-lsp'
+
+----TREESITTER----------------------------------
+Plug 'nvim-treesitter/nvim-treesitter'
+
+vim.call('plug#end')
+
